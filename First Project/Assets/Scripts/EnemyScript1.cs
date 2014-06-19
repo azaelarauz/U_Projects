e941069b002b84c1579051f380ff7e3727e4bb8e
azaelarauz <azael.arauz@gmail.com>
2014-06-19 16:34:47 -0500

    .

---
+++ First Project/Assets/Scripts/EnemyScript1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript1 : MonoBehaviour {
+
+	// Use this for initialization
+	private WeaponScript[] weapons;
+
+	void Awake()
+	{
+		// Retrieve the weapon only once
+		weapons = GetComponentsInChildren<WeaponScript>();
+	}
+
+	void Update()
+	{
+		// Auto-fire
+		foreach(WeaponScript weapon in weapons)
+		if (weapons != null && weapon.CanAttack)
+		{
+			weapon.Attack(true);
+		}
+	}
+}
